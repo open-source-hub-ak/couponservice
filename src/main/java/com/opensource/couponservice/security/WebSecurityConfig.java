5440fb5eecba42f1b13f91f9e69b0a1bbc6d4123
aakash <aakash>
2021-01-31 21:21:59 +0530

    implemented authorize request and basi auth

--- src/main/java/com/opensource/couponservice/security/WebSecurityConfig.java
+++ src/main/java/com/opensource/couponservice/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.opensource.couponservice.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,7 +24,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.httpBasic(); // what type of authentication form based , basic etc
+		
+		//Here how we are authorizing request
+		http.authorizeRequests().mvcMatchers(HttpMethod.GET, "/couponapi/coupons").hasAnyRole("USER", "ADMIN")
+				.mvcMatchers(HttpMethod.POST, "/couponapi/coupons").hasRole("ADMIN").and().csrf().disable();
 	}
 
 	@Bean
