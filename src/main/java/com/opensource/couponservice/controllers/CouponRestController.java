9d325e51cb880422fed174acdba705143ac809dc
aakash <aakash>
2021-01-31 17:43:43 +0530

    Created api for POST coupon and GET coupon

---
+++ src/main/java/com/opensource/couponservice/controllers/CouponRestController.java
@@ -0,0 +1,29 @@
+package com.opensource.couponservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.opensource.couponservice.model.Coupon;
+import com.opensource.couponservice.repos.CouponRepo;
+
+@RestController
+@RequestMapping("/couponapi")
+public class CouponRestController {
+
+	@Autowired
+	private CouponRepo couponRepo;
+
+	@PostMapping("/coupons")
+	public Coupon createCoupon(Coupon coupon) {
+		return couponRepo.save(coupon);
+	}
+	
+	@GetMapping("/coupons/{code}")
+	public Coupon getCoupon(String code) {
+		return couponRepo.findByCode(code);
+	}
+	
+}
