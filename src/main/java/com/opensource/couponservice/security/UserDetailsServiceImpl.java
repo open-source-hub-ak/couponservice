c4b2183c64087d8d89a6259e36259206f43e59f6
aakash <aakash>
2021-01-31 20:57:04 +0530

    Implement UserDetailService Impl

---
+++ src/main/java/com/opensource/couponservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.opensource.couponservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.opensource.couponservice.model.User;
+import com.opensource.couponservice.repos.UserRepo;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepo userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found for email " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				user.getRoles());
+	}
+
+}
