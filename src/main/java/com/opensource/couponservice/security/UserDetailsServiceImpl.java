942706c631990a252bcec32656235c06f554befb
aakash <aakash>
2021-01-31 21:27:35 +0530

    Bug fix in Security config

--- src/main/java/com/opensource/couponservice/security/UserDetailsServiceImpl.java
+++ src/main/java/com/opensource/couponservice/security/UserDetailsServiceImpl.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.opensource.couponservice.model.User;
 import com.opensource.couponservice.repos.UserRepo;
 
+@Component
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
