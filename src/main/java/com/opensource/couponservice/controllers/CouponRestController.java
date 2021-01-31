aed316e28353fd5cbfc18db715a3679898ae24ea
aakash <aakash>
2021-01-31 17:47:28 +0530

    Big Fixed for Request body and path variable

--- src/main/java/com/opensource/couponservice/controllers/CouponRestController.java
+++ src/main/java/com/opensource/couponservice/controllers/CouponRestController.java
@@ -2,7 +2,9 @@ package com.opensource.couponservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +19,13 @@ public class CouponRestController {
 	private CouponRepo couponRepo;
 
 	@PostMapping("/coupons")
-	public Coupon createCoupon(Coupon coupon) {
+	public Coupon createCoupon(@RequestBody Coupon coupon) {
 		return couponRepo.save(coupon);
 	}
-	
+
 	@GetMapping("/coupons/{code}")
-	public Coupon getCoupon(String code) {
+	public Coupon getCoupon(@PathVariable("code") String code) {
 		return couponRepo.findByCode(code);
 	}
-	
+
 }
