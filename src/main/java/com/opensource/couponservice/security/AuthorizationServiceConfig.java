b095afd8c1fb9357e31271a4bc50d4ecc268ff02
aakash <aakash>
2021-02-02 16:53:22 +0530

    Created Authorization server endpoint

---
+++ src/main/java/com/opensource/couponservice/security/AuthorizationServiceConfig.java
@@ -0,0 +1,27 @@
+package com.opensource.couponservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServiceConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(new InMemoryTokenStore()).authenticationManager(authenticationManager)
+				.userDetailsService(userDetailsService);
+	}
+}
