6215bcf5a5e94569c6d563ea5061f0c03087de36
aakash <aakash>
2021-02-02 17:04:41 +0530

    Authorization Server implemented

--- src/main/java/com/opensource/couponservice/security/AuthorizationServiceConfig.java
+++ src/main/java/com/opensource/couponservice/security/AuthorizationServiceConfig.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import com.opensource.couponservice.util.SecurityConstants;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,9 +23,21 @@ public class AuthorizationServiceConfig extends AuthorizationServerConfigurerAda
 	@Autowired
 	private UserDetailsService userDetailsService;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(new InMemoryTokenStore()).authenticationManager(authenticationManager)
 				.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		clients.inMemory().withClient(SecurityConstants.CLIENT_ID)
+				.secret(passwordEncoder.encode(SecurityConstants.CLIENT_SECRET))
+				.authorizedGrantTypes("password", "refresh_token").scopes("read", "write")
+				.resourceIds(SecurityConstants.RESOURCE_ID);
 	}
 }
---
+++ src/main/java/com/opensource/couponservice/util/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.opensource.couponservice.util;
+
+public class SecurityConstants {
+
+	public static final String RESOURCE_ID = "couponservice";
+	public static final String CLIENT_ID = "couponclientapp";
+	public static final String CLIENT_SECRET = "9999";
+
+}
